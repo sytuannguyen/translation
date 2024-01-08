6339ae4e11e92449ebdfc705fcead29875963486
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2024-01-08 10:17:25 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from transformers import MarianTokenizer, MarianMTModel
+
+main():
+	st.Title('French to English tranlation, by Dr Tuan Nguyen-Sy')
+
+	input_text = st.text_input('Enter a sentense in French here','La vie est belle')
+
+	st.button('Translate'):
+		st.text('Translating ...')
+		
+		model_name = 'Helsinki-NLP/opus-mt-fr-en'
+		tokenizer = MarianTokenizer.from_pretrained(model_name)
+		model = MarianMTModel.from_pretrained(model_name)
+
+		input_token_numbers = tokenizer.encode(input_text,output_tensors='pt')
+		output_token_numbers = model.generate(input_token_numbers)
+
+		output_text = tokenizer.decode(output_token_numbers[0], skip_special_tokens=True)
+
+		st.success(output_text)
+	
+if __name__ == __main__():
+	main()
